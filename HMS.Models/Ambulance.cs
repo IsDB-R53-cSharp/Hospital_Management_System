149fc10724feb3e560aaa49c278d56f0fbfe2434
Md.Ataur Rahman <142331145+ataur481209@users.noreply.github.com>
2023-09-14 13:28:44 +0600

    Ambulance.cs
    HMS.Models_Ambulance.cs

--- HMS.Models/Ambulance.cs
+++ HMS.Models/Ambulance.cs
@@ -12,14 +12,21 @@ namespace HMS.Models
     {
         [Key]
         public int AmbulanceID { get; set; }
+        [Required(ErrorMessage = "Please enter Ambulance Number")]
+        [Display(Name = "Ambulance Number")]
         public string AmbulanceNumber { get; set; } = default!;
         //Driver Phone Number{Required}
         [Required(ErrorMessage = "Please enter phone number")]
-        [Display(Name = "Phone")]
-        [DataType(DataType.PhoneNumber)]
+        [Display(Name = "Driver Phone Number")]
         public string PhoneNumber { get; set; } = default!;
+        [Required(ErrorMessage = "Please enter Driving Liense")]
+        [Display(Name = "Driving Liense")]
         public string DrivingLiense { get; set; } = default!;
+        [Required(ErrorMessage = "Please enter Driver Name"),StringLength(50)]
+        [Display(Name = "Driver Name")]
         public string DriverName { get; set; } = default!;
+        [Required(ErrorMessage = "Please enter Last Location"), StringLength(200)]
+        [Display(Name = "Last Location")]
         public string LastLocation { get; set; } = default!;
         public bool Availability { get; set; }
     }
