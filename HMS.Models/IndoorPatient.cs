a8d4c7158cd570509bf74c8086edd585bbd159ac
Abdullah <arsk.abdullah@gmail.com>
2023-09-27 06:01:42 +0600

    new bill table

---
+++ HMS.Models/IndoorPatient.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using HMS.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HMS.Models
+{
+    public class IndoorPatient
+    {
+        [Key]
+        public int IndoorPatientID { get; set; }
+
+        public int PatientID { get; set; }
+        public int WardID { get; set; }
+
+        [ForeignKey("MedicalRecords")]
+        public int? MedicalRecordsID { get; set; }
+
+        public bool IsDead { get; set; }
+
+        [StringLength(200)]
+        [Display(Name = "Referred By")]
+        public string ReferredBy { get; set; }
+
+        [Display(Name = "Bed Number")]
+        public int BedNumber { get; set; }
+
+        public string NIDnumber { get; set; }
+
+        [StringLength(500)]
+        [Display(Name = "Insurance Information")]
+        public string? InsuranceInfo { get; set; }
+
+        [StringLength(500)]
+        [Display(Name = "Admission Notes")]
+        public string? AdmissionNotes { get; set; }
+
+
+        // Navigation property for the patient
+        [NotMapped]
+        public virtual PatientRegister PatientRegisters { get; set; }
+
+        // Navigation property for medical records
+        [NotMapped]
+        public virtual MedicalRecords MedicalRecords { get; set; }
+    }
+}
---
+++ HMS.Models/TestBill.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HMS.Models
+{
+    public class TestBill
+    {
+        [Key]
+        public int TestBillID { get; set; }
+
+
+        public int TestID { get; set; }
+        public int PatientID { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal TestPrice { get; set; }
+
+        [ForeignKey("Bill")]
+        public int BillID { get; set; }
+
+        // Navigation property for Bill
+        public virtual Bill Bill { get; set; }
+    }
+}
