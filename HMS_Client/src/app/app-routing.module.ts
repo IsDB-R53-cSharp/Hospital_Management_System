cfafe762a591c207fc97d71ba623b12d8b425aca
manjurul0992 <monjurulislam327@gmail.com>
2023-10-20 12:20:26 +0600

    dashboard

--- HMS_Client/src/app/app-routing.module.ts
+++ HMS_Client/src/app/app-routing.module.ts
@@ -6,6 +6,8 @@ import { SurgeryComponent } from './componenets/surgery/surgery.component';
 import { WardCabinComponent } from './componenets/ward-cabin/ward-cabin.component';
 import { NurseComponent } from './componenets/nurse/nurse.component';
 import { DoctorComponent } from './componenets/doctor/doctor.component';
+import { PageNotFoundComponent } from './modules/deshboard/page-not-found/page-not-found.component';
+import { DepartmentComponent } from './modules/department/department.component';
 
 const routes: Routes = [
   {path:'bed',component:BedComponent},
@@ -13,7 +15,14 @@ const routes: Routes = [
   {path:'surgery',component:SurgeryComponent},
   {path:'wardCabin',component:WardCabinComponent},
   {path:'nurse',component:NurseComponent},
-  {path:'doctor',component:DoctorComponent},
+  { path: 'doctor', component: DoctorComponent },
+
+
+
+
+
+  { path: "department-designation", component: DepartmentComponent },
+  { path: '**', pathMatch: 'full', component: PageNotFoundComponent },
 ];
 
 @NgModule({
--- HMS_Client/src/app/app.component.ts
+++ HMS_Client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HMS_Client';
+  public loaderMessage: string = "loading test";
+
+  public hideNav: boolean = false;
+  constructor(private router: Router) {
+    // router.events.subscribe((url:any) =>{
+    //      if(router.url=='/sign-in' || router.url=='/sign-up'){
+    //       this.hideNav=true;
+    //      }
+
+    //     });
+  }
 }
